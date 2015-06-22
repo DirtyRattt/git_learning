fe5591932227229094a8274f1f1ff52c8d29fcec
DirtyRattt <katarzyna.zurawiecka@hotmail.com>
2015-06-22 10:34:23 +0200

    update 3

--- OpenCV_app.py
+++ OpenCV_app.py
@@ -5,13 +5,13 @@ camera = cv2.VideoCapture(0)
 
 while True:
 	ret, frame = camera.read()
-	cv2.imshow('image', frame)
 	key = cv2.waitKey(1)
 
 	if key == 27:
 		break
 
-frame = cv2.cvtColor(frame, cv2.cvtCOLOR_BGR2GRAY)
-cv2.imshow('image', frame)
+	frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	ret, frame = cv2.threshold(frame,127,255,cv2.THRESH_TRUNC)
+	cv2.imshow('image', frame)
 
 cv2.destroyAllWindows()
